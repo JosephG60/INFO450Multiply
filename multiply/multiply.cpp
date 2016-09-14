9935fe0880f76eccdc4091be5c0a599a75bd5e27
Joseph Gay <Joseph Gay>
2016-09-13 22:17:45 -0400

    first commit

--- multiply/multiply.cpp
+++ multiply/multiply.cpp
@@ -1,11 +1,37 @@
 // multiply.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+// This function prints the multiplication table up to "n".
+void printTable(int n)
+{
+	int i, j;
+
+	cout << 0 << "\t";
+	for (i = 0; i <= n; i++)
+	{
+		cout << i << "\t";
+	}
+
+	cout << endl;
+	for (i = 0; i <= 10; i++)
+	{
+		cout << i << "\t";
+		for (j = 0; j <= n; j++)
+			cout << i*j << "\t";
+		cout << endl;
+
+	}
+}
 
 int main()
 {
-    return 0;
+	int n;
+	cout << "Please enter a number: " << endl;
+	cin >> n;
+	printTable(n);
 }
 
